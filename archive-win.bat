33f093eadc450946aed03c16fcc24733597f92fa
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-08-13 02:42:47 +0430

    Rearranged the ".gitignore" file. Created the batch file to archive non-git files on Windows.

---
+++ archive-win.bat
@@ -0,0 +1,4 @@
+echo Creating the archive on %date% at %time% > %userprofile%\Desktop\list.txt
+echo Archive contents: >> %userprofile%\Desktop\list.txt
+for /F "delims=" %%A in ('dir /s /b^|findstr "^.*\\bin\\.*$ ^.*\\uploads\\.* ^.*\.vmcompiled\..* ^.*\.jpg$ ^.*\.JPG$ ^.*\.png$ ^.*\.PNG$ ^.*\.ttf$ ^.*\.eot$ ^.*\.woff$ ^.*\.woff2$ ^.*\.otf$ ^.*\.svg$ ^.*\.gif$"') do 7za a -tzip %userprofile%\Desktop\bin.zip -spf2 %%~fA && echo %%~fA >> %userprofile%\Desktop\list.txt
+for %%A in (%userprofile%\Desktop\bin.zip.tmp*) do del %%~fA
