1097039683b829a6b99a501fb9811d50ee1e3161
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-09-05 09:55:48 +0430

    Customized VM-WPT-Business as per ordered.

--- archive-win.bat
+++ archive-win.bat
@@ -1,3 +1,3 @@
-echo Creating the archive %userprofile%\Desktop\bin.zip on %date% at %time% >> %userprofile%\Desktop\list.txt
+echo Creating the archive %userprofile%\Desktop\bin.zip on %date% at %time% > %userprofile%\Desktop\list.txt
 echo Archive contents: >> %userprofile%\Desktop\list.txt
 for /F "delims=" %%A in ('dir /s /b ^| findstr "^.*\\bin\\.*$ ^.*\\uploads\\.* ^.*\.vmcompiled\..* ^.*\.jpg$ ^.*\.JPG$ ^.*\.png$ ^.*\.PNG$ ^.*\.ttf$ ^.*\.eot$ ^.*\.woff$ ^.*\.woff2$ ^.*\.otf$ ^.*\.svg$ ^.*\.gif$"') do 7za a -tzip %userprofile%\Desktop\bin.zip -spf2 %%~fA && echo %%~fA >> %userprofile%\Desktop\list.txt
