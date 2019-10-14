9e8f1761c88a8c1f6cf897f6f473ed5b4594a4f4
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-10-14 07:34:23 +0330

    archive-win.bat>>> improved archive creation mechanism

--- archive-win.bat
+++ archive-win.bat
@@ -1,3 +1,6 @@
-echo Creating the archive %userprofile%\Desktop\bin.zip on %date% at %time% > %userprofile%\Desktop\list.txt
-echo Archive contents: >> %userprofile%\Desktop\list.txt
-for /F "delims=" %%A in ('dir /s /b ^| findstr ".*\\bin\\.* .*\\uploads\\.* .*\.jpg$ .*\.JPG$ .*\.png$ .*\.PNG$ .*\.ttf$ .*\.eot$ .*\.woff$ .*\.woff2$ .*\.otf$ .*\.svg$ .*\.gif$"') do 7za u -tzip %userprofile%\Desktop\bin.zip -spf2 %%~fA && echo %%~fA >> %userprofile%\Desktop\list.txt
+del %userprofile%\Desktop\list.txt
+del %userprofile%\Desktop\bin.zip
+echo Creating the archive %userprofile%\Desktop\bin.zip on %date% at %time%
+echo Archive contents:
+for /F "delims=" %%A in ('dir /s /b ^| findstr ".*\\bin\\.* .*\\uploads\\.* .*\.jpg$ .*\.JPG$ .*\.png$ .*\.PNG$ .*\.ttf$ .*\.eot$ .*\.woff$ .*\.woff2$ .*\.otf$ .*\.svg$ .*\.gif$"') do echo %%~fA >> %userprofile%\Desktop\list.txt
+7za a -uz0 -tzip -spf2 %userprofile%\Desktop\bin.zip @%userprofile%\Desktop\list.txt
