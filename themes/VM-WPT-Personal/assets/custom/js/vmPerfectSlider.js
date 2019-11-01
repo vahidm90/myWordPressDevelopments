677bd0063077bb3ce69102627e04e88774b70368
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-11-01 22:56:37 +0330

    animations.list>>> created the list containing all possible entrance & exit animations. vmPerfectSlider.js>>> improved variables, improved functions.

--- themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
+++ themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
@@ -1,130 +1,147 @@
 (function ($, window, document, undefined) {
+
     var vmPerfectSlider = function (element, options) {
         this.$slider = $(element);
         this.options = options;
     };
+
     vmPerfectSlider.prototype =
         {
+
             init: function () {
 
                 this.settings = $.extend({}, this.defaults, this.options);
-                this.slides = this.$slider.children(this.settings.elementSelector);
-                this.slidesCount = this.slides.length;
-                this.interval = [];//Ovveride config.interval
-                //TODO: if you can proceed as does this plugin, add the interval option.
-                this.current = 0; //first slide
-//TODO: add the options to customize indicator markup/css.
-                var $dots = $('<div class="anim-dots"></div>');
-                var temp = this.slidesCount;
-                while (temp--) {
-                    $dots.append("<span></span>");
+                this.allEntranceAnimations = 'bounceIn bounceInDown  bounceInLeft  bounceInRight  bounceInUp  slideInDown slideInLeft slideInRight slideInUp fadeIn fadeInDown fadeInDownBig fadeInLeft fadeInLeftBig fadeInRight fadeInRightBig fadeInUp fadeInUpBig rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight flipInX flipInY lightSpeedIn zoomIn  zoomInDown  zoomInLeft  zoomInRight  zoomInUp  jackInTheBox  rollIn';
+                this.allExitanimations = 'bounceOut bounceOutDown  bounceOutLeft  bounceOutRight  bounceOutUp  fadeOut fadeOutDown  fadeOutDownBig  fadeOutLeft  fadeOutLeftBig  fadeOutRight  fadeOutRightBig  fadeOutUp  fadeOutUpBig  flipOutX flipOutY  lightSpeedOut rotateOut rotateOutDownLeft  rotateOutDownRight  rotateOutUpLeft  rotateOutUpRight  hingeOut rollOut  zoomOut  zoomOutDown  zoomOutLeft  zoomOutRight  zoomOutUp  slideOutDown slideOutLeft  slideOutRight  slideOutUp';
+                this.$slides = this.$slider.children('.' + this.settings.slideClass);
+                this.currentSlide = 0;
+                var $dots = $('<div class="' + this.settings.indicatorWrapClass + '"></div>');
+
+                for ( var i = 0 ; this.$slides.length > i; i++ ) {
+
+                    var $theSlide = this.$slides.eq(i);
+                    if (0 >= $theSlide.find('.' + this.settings.lastElementClass).length) {
+                        $theSlide.find('.' + this.settings.elementClass).last().addClass(this.settings.lastElementClass);
+                    } else if (1 < $theSlide.find('.' + this.settings.lastElementClass).length) {
+                        $theSlide.find('.' + this.settings.elementClass).removeClass(this.settings.lastElementClass);
+                        $theSlide.find('.' + this.settings.elementClass).last().addClass(this.settings.lastElementClass);
+                    }
+                    $dots.prepend('<span class="' + this.settings.navigationClass + '" data-go-to-slide="' + i + '"></span>');
+
                 }
+
                 $dots.appendTo(this.$slider);
-                this.slides.eq(this.current).addClass('current-slide');
-
-                this.$nav = this.$slider.find('.slide-nav');
-
-                this.$dots = this.$slider.find('.anim-dots>span');
-                //TODO: add the options to customize navigation arrows markup/css.
-                this.$navNext = this.$slider.find(".anim-arrows-next");
-                this.$navPrev = this.$slider.find(".anim-arrows-prev");
-
+                this.$nav = this.$slider.find('.' + this.settings.navigationClass);
+                //TODO: make next/previous buttons dynamic too.
                 this.loadEvents();
-                this.navigate(this.current);
+                this.navigate(this.currentSlide);
                 this.autoplay();
+
             },
+
             loadEvents: function () {
                 var that = this;
                 //TODO: don't forget relevant data on arrows/dots.
-                this.$nav.on('click.vmPS', function (e) {
-                    if (!$(this).data('go-to')) {
+                this.$nav.on('click', function (e) {
+                    if (!$(this).data('go-to-slide')) {
                         return;
                     }
                     e.preventDefault();
-                    switch ($(this).data('go-to')) {
+                    switch ($(this).data('go-to-slide')) {
                         default:
-                            that.navigate(parseInt($(this).data('where'), 10));
+                            that.navigate(parseInt($(this).data('go-to-slide'), 10));
                             break;
                         case 'N' :
-                            that.navigate(that.current + 1);
+                            that.navigate(that.currentSlide - 1);
                             break;
                         case 'P' :
-                            that.navigate(that.current - 1);
+                            that.navigate(that.currentSlide + 1);
                             break;
                     }
                 });
-                this.slides.on( 'slideEnd.vmPS', '[data-vmPS-last="1"]', function () {
-                    this.navigate(++this.current);
-                });
-                this.slides.filter('[data-vmPS-domino="1"]').children(':not(:last-child)').on('animationend', function () {
-                    var $nextElement = $(this).next(),
-                        animation = $nextElement.data('vmPS-animation')?$nextElement.data('vmPS-animation'):this.settings.elementEntranceAnimation,
-                        delay = $nextElement.data('vmPS-entrance-delay')?parseInt($nextElement.data('vmPS-entrance-delay'), 10):this.settings.elementEntranceDelay;
+            },
 
-                    $nextElement.addClass('animated')
-                })
-            },
             navigate: function (slide) {
-            	var next;
-            	if ( -1 === slide ) {
-            		next = this.slidesCount - 1;
-				} else if ( this.slidesCount === slide ) {
-            		next = 0;
-				} else if ( (-1 < slide)&&( this.slidesCount > slide) ) {
-            		next = slide;
-				} else {
-            		return;
-				}
-            	if (this.slides.eq(this.current).data('vmPS-domino')) {
-            	    this.slideDomino();
+
+                this.$slides.eq(this.currentSlide).addClass('rolling');
+                var next;
+                if (-1 === slide) {
+                    next = this.$slides.length - 1;
+                } else if (this.$slides.length === slide) {
+                    next = 0;
+                } else if ((-1 < slide) && (this.slidesCount > slide)) {
+                    next = slide;
                 } else {
-                    this.slideAnimations(this.current);
+                    return;
                 }
-            	this.navigate(next);
+                if (this.slides.eq(this.currentSlide).data('vmPS-domino')) {
+                    this.slideDomino();
+                } else {
+                    this.slideAnimate(this.currentSlide);
+                }
+                this.navigate(next);
                 this.updateIndicators();
+
             },
+
             slideDomino: function () {
-                this.slides.eq(this.current).children().each(function () {
+
+                var that = this, $elements = this.$slides.eq(this.currentSlide).find('.' + this.settings.elementClass);
+                $elements.each( function () {
+                })
+                $elements.first().hasClass()
+                this.$slides.on('slideEnd.vmPS', '[data-vmPS-last="1"]', function () {
+                    this.navigate(++this.currentSlide);
+                });
+
+                this.$slides.filter('[data-vmPS-domino="1"]').find('.' + this.settings.elementClass + ':not(.'+this.settings.lastElementClass+')').on('animationend', function () {
+                    var $nextElement = $(this).next(),
+                        animation = $nextElement.data('vmPS-animation') ? $nextElement.data('vmPS-animation') : that.settings.elementEntranceAnimation;
+                    if ($nextElement.is('[class~="delay-"]')) {
+                        $nextElement.addClass('animated ' + animation);
+                    } else {
+                        var delay = ($nextElement.data('vmPS-entrance-delay')) ? parseInt($nextElement.data('vmPS-entrance-delay'), 10) : that.settings.elementEntranceDelay;
+                        $nextElement.css('animation-delay', delay).addClass('animated ' + animation);
+                    }
+                });
+                this.$slides.find('.'+this.settings.lastElementClass).on('animationend', function () {
+
+                });
+                this.slides.eq(this.currentSlide).children().each(function () {
                     if ($(this).data('vmPS-animation')) {
                         addClass()
                     }
                 })
-            }
+
+            },
+
+            defaults :
+                {
+                    slideClass: 'slide',
+                    indicatorWrapClass: 'indicator',
+                    navigationClass: 'slide-nav',
+                    elementClass: 'slide-element',
+                    elementAnimationDuration: .1,
+                    elementEntranceAnimation: 'fadeIn',
+                    elementEntranceDelay: 0,
+                    elementExitAnimation: 'fadeOut',
+                    lastElementStayTime: 2,
+                    lastElementClass: 'last-element',
+                }
+
         };
+
     $.fn.vmPerfectSlider = function (options) {
 
-        var settings = $.extend({}, $.fn.vmPerfectSlider.defaults, options);
-
-        return this.each(function () {
-            var $slider = $(this),
-                $slides = $slider.children(settings.slideSelector);
-            $slides.each(function () {
-                var $sliders = $(this).children(settings.elementSelector);
-                if ($(this).data('vmPS-domino')) {
-                    $(this).children(':not(:last-child)').on('animationend', function () {
-                        var $nextElement = $(this).next();
-                        $nextElement.addClass($nextElement.data('vmPS-animation') + ' animated')
-                    })
-                }
-                $(this).children(settings.elementSelector).each(function () {
-                    if ($(this).data('vmPS-animation')) {
-                        $(this).addClass()
-                    }
-                })
-            });
+        return this.each(function()
+        {
+            var instance	=	$.data(this,"vmPerfectSlider");
+            if (!instance)
+            {
+                $.data(this,"vmPerfectSlider",new vmPerfectSlider(this,options).init());
+            }
         });
 
     };
-
-    $.fn.vmPerfectSlider.defaults = {
-        slideSelector: '.slide',
-        elementSelector: '*',
-        elementAnimationDuration: .1,
-        elementEntranceAnimation: 'fadeIn',
-        elementEntranceDelay: 0,
-        elementExitAnimation: 'fadeOut',
-        elementStay: 2,
-        lastElementData: 'vmPS-last',
-    }
 
 }(jQuery));
