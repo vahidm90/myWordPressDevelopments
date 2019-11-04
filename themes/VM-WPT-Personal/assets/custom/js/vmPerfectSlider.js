735fae57f06c3f2a6ba79c68c69b3a8686640708
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-11-04 18:33:04 +0330

    ...

--- themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
+++ themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
@@ -11,97 +11,129 @@
             init: function () {
 
                 this.settings = $.extend({}, this.defaults, this.options);
-                this.allEntranceAnimations = 'bounceIn bounceInDown  bounceInLeft  bounceInRight  bounceInUp  slideInDown slideInLeft slideInRight slideInUp fadeIn fadeInDown fadeInDownBig fadeInLeft fadeInLeftBig fadeInRight fadeInRightBig fadeInUp fadeInUpBig rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight flipInX flipInY lightSpeedIn zoomIn  zoomInDown  zoomInLeft  zoomInRight  zoomInUp  jackInTheBox  rollIn';
-                this.allExitanimations = 'bounceOut bounceOutDown  bounceOutLeft  bounceOutRight  bounceOutUp  fadeOut fadeOutDown  fadeOutDownBig  fadeOutLeft  fadeOutLeftBig  fadeOutRight  fadeOutRightBig  fadeOutUp  fadeOutUpBig  flipOutX flipOutY  lightSpeedOut rotateOut rotateOutDownLeft  rotateOutDownRight  rotateOutUpLeft  rotateOutUpRight  hingeOut rollOut  zoomOut  zoomOutDown  zoomOutLeft  zoomOutRight  zoomOutUp  slideOutDown slideOutLeft  slideOutRight  slideOutUp';
+                this.allEnterAnims = 'bounceIn bounceInDown bounceInLeft bounceInRight bounceInUp fadeIn fadeInDown fadeInDownBig fadeInLeft fadeInLeftBig fadeInRight fadeInRightBig fadeInUp fadeInUpBig flipInX flipInY lightSpeedIn rollIn rotateIn rotateInDownLeft rotateInDownRight rotateInUpLeft rotateInUpRight slideInDown slideInLeft slideInRight slideInUp zoomIn zoomInDown zoomInLeft zoomInRight zoomInUp jackInTheBox';
+                this.allExitAnims = 'bounceOut bounceOutDown bounceOutLeft bounceOutRight bounceOutUp fadeOut fadeOutDown fadeOutDownBig fadeOutLeft fadeOutLeftBig fadeOutRight fadeOutRightBig fadeOutUp fadeOutUpBig flipOutX flipOutY lightSpeedOut rollOut rotateOut rotateOutDownLeft rotateOutDownRight rotateOutUpLeft rotateOutUpRight slideOutDown slideOutLeft slideOutRight slideOutUp zoomOut zoomOutDown zoomOutLeft zoomOutRight zoomOutUp hingeOut';
+                this.allEnterAnimsArr = this.allEnterAnims.split(' ');
+                this.allExitAnimsArr = this.allExitAnims.split(' ');
                 this.$slides = this.$slider.children('.' + this.settings.slideClass);
-                this.currentSlide = 0;
-                var $dots = $('<div class="' + this.settings.indicatorWrapClass + '"></div>');
+                if (0 >= this.$slides.length) {
+                    throw new Error('No elements with the CSS class "' + this.settings.slideClass + '" found!' );
+                }
+                this.curSlide = 0;
+                var $indicator = $('<div class="' + this.settings.indicatorWrapClass + '"></div>');
 
                 for ( var i = 0 ; this.$slides.length > i; i++ ) {
 
-                    var $theSlide = this.$slides.eq(i),
-                        $slideElements = $theSlide.find('.' + this.settings.elementClass);
-                    if (0 >= $slideElements.length) {
-
-                        if ($theSlide.hasClass())
+                    var that = this,
+                        $slide = this.$slides.eq(i),
+                        $elements = $slide.find('.' + this.settings.elemClass),
+                        $lastElem = $elements.filter('.' + this.settings.lastElemClass);
+                    if (0 >= $elements.length) {
+                        $slide.data('animateSelf', 1);
+                        var slideHasEnterAnim = that.allEnterAnimsArr.some(function (anim) {
+                            return anim===$slide.data('enter');
+                        }),
+                        slideHasExitAnim = that.allExitAnimsArr.some(function (anim) {
+                            return anim===$slide.data('exit');
+                        });
+                        if(!slideHasEnterAnim) {
+                            $slide.data('enter', that.settings.elemEnterAnim);
+                        }
+                        if(!slideHasExitAnim) {
+                            $slide.data('exit', that.settings.elemExitAnim);
+                        }
+                    } else {
+                        $slide.data('animateSelf', 0);
+                        $elements.each(function () {
+                            var elemHasEnterAnim = that.allEnterAnimsArr.some(function (anim) {
+                                return anim===$(this).data('enter');
+                            }),
+                            elemHasExitAnim = that.allExitAnimsArr.some(function (anim) {
+                                return anim===$(this).data('exit');
+                            });
+                            if(!elemHasEnterAnim) {
+                                $(this).data('enter',that.settings.elemEnterAnim);
+                            }
+                            if(!elemHasExitAnim) {
+                                $(this).data('exit', that.settings.elemExitAnim);
+                            }
+                        });
+                        if (0 >= $lastElem.length) {
+                            $elements.last().addClass(this.settings.lastElemClass);
+                        } else if (1 < $lastElem.length) {
+                            $lastElem.removeClass(this.settings.lastElemClass);
+                            $elements.last().addClass(this.settings.lastElemClass);
+                        }
                     }
-                    if (0 >= $theSlide.find('.' + this.settings.lastElementClass).length) {
-                        $theSlide.find('.' + this.settings.elementClass).last().addClass(this.settings.lastElementClass);
-                    } else if (1 < $theSlide.find('.' + this.settings.lastElementClass).length) {
-                        $theSlide.find('.' + this.settings.elementClass).removeClass(this.settings.lastElementClass);
-                        $theSlide.find('.' + this.settings.elementClass).last().addClass(this.settings.lastElementClass);
-                    }
-                    $dots.prepend('<span class="' + this.settings.navigationClass + '" data-go-to-slide="' + i + '"></span>');
+                    $indicator.prepend('<span class="' + this.settings.navClass + '" data-go-to-slide="' + i + '"></span>');
 
                 }
 
-                $dots.appendTo(this.$slider);
-                this.$nav = this.$slider.find('.' + this.settings.navigationClass);
+                $indicator.appendTo(this.$slider);
+                this.$nav = this.$slider.find('.' + this.settings.navClass);
                 //TODO: make next/previous buttons dynamic too.
                 this.loadEvents();
-                this.navigate(this.currentSlide);
+                this.navigate(this.curSlide);
                 this.autoplay();
 
             },
 
             loadEvents: function () {
                 var that = this;
-                //TODO: don't forget relevant data on arrows/dots.
+                //TODO: don't forget relevant data on arrows/indicators.
                 this.$nav.on('click', function (e) {
-                    if (!$(this).data('go-to-slide')) {
+                    if (!$(this).data('goToSlide')) {
                         return;
                     }
                     e.preventDefault();
-                    switch ($(this).data('go-to-slide')) {
+                    switch ($(this).data('goToSlide')) {
                         default:
-                            that.navigate(parseInt($(this).data('go-to-slide'), 10));
+                            that.navigate($(this).data('goToSlide'));
                             break;
                         case 'N' :
-                            that.navigate(that.currentSlide - 1);
+                            that.navigate(that.curSlide - 1);
                             break;
                         case 'P' :
-                            that.navigate(that.currentSlide + 1);
+                            that.navigate(that.curSlide + 1);
                             break;
                     }
                 });
             },
 
-            navigate: function (slide) {
-
-                this.$slides.eq(this.currentSlide).addClass('rolling');
-                var next;
-                if (-1 === slide) {
-                    next = this.$slides.length - 1;
-                } else if (this.$slides.length === slide) {
-                    next = 0;
-                } else if ((-1 < slide) && (this.slidesCount > slide)) {
-                    next = slide;
+            navigate: function (curSlide) {
+                //TODO:update indicators.
+                this.$slides.eq(this.curSlide).addClass('rolling');
+                var nextSlide;
+                if (-1 === curSlide) {
+                    nextSlide = this.$slides.length - 1;
+                } else if (this.$slides.length === curSlide) {
+                    nextSlide = 0;
+                } else if ((-1 < curSlide) && (this.slidesCount > curSlide)) {
+                    nextSlide = curSlide;
                 } else {
                     return;
                 }
-                if (this.slides.eq(this.currentSlide).data('vmPS-domino')) {
+                if (this.slides.eq(this.curSlide).data('domino')) {
                     this.slideDomino();
                 } else {
-                    this.slideAnimate(this.currentSlide);
+                    //TODO:the function to play elements' animations
                 }
-                this.navigate(next);
-                this.updateIndicators();
-
+                this.navigate(nextSlide);
             },
 
             slideDomino: function () {
 
-                var that = this, $elements = this.$slides.eq(this.currentSlide).find('.' + this.settings.elementClass);
+                var that = this, $elements = this.$slides.eq(this.curSlide).find('.' + this.settings.elemClass);
                 $elements.each( function () {
                 })
                 $elements.first().hasClass()
                 this.$slides.on('slideEnd.vmPS', '[data-vmPS-last="1"]', function () {
-                    this.navigate(++this.currentSlide);
+                    this.navigate(++this.curSlide);
                 });
 
-                this.$slides.filter('[data-vmPS-domino="1"]').find('.' + this.settings.elementClass + ':not(.'+this.settings.lastElementClass+')').on('animationend', function () {
+                this.$slides.filter('[data-vmPS-domino="1"]').find('.' + this.settings.elemClass + ':not(.'+this.settings.lastElemClass+')').on('animationend', function () {
                     var $nextElement = $(this).next(),
-                        animation = $nextElement.data('vmPS-animation') ? $nextElement.data('vmPS-animation') : that.settings.elementEntranceAnimation;
+                        animation = $nextElement.data('vmPS-animation') ? $nextElement.data('vmPS-animation') : that.settings.elemEnterAnim;
                     if ($nextElement.is('[class~="delay-"]')) {
                         $nextElement.addClass('animated ' + animation);
                     } else {
@@ -109,10 +141,10 @@
                         $nextElement.css('animation-delay', delay).addClass('animated ' + animation);
                     }
                 });
-                this.$slides.find('.'+this.settings.lastElementClass).on('animationend', function () {
+                this.$slides.find('.'+this.settings.lastElemClass).on('animationend', function () {
 
                 });
-                this.slides.eq(this.currentSlide).children().each(function () {
+                this.slides.eq(this.curSlide).children().each(function () {
                     if ($(this).data('vmPS-animation')) {
                         addClass()
                     }
@@ -124,14 +156,14 @@
                 {
                     slideClass: 'slide',
                     indicatorWrapClass: 'indicator',
-                    navigationClass: 'nav',
-                    elementClass: 'element',
+                    navClass: 'nav',
+                    elemClass: 'element',
                     elementAnimationDuration: .05,
-                    elementEntranceAnimation: 'fadeIn',
+                    elemEnterAnim: 'fadeIn',
                     elementEntranceDelay: 0,
-                    elementExitAnimation: 'fadeOut',
+                    elemExitAnim: 'fadeOut',
                     lastElementStayTime: 2,
-                    lastElementClass: 'last',
+                    lastElemClass: 'last',
                 }
 
         };
