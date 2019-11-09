d464abb3500a5d1b7227b6c184076720a22610e3
vahidm90 <49893905+vahidm90@users.noreply.github.com>
2019-11-09 07:30:24 +0330

    ...

--- themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
+++ themes/VM-WPT-Personal/assets/custom/js/vmPerfectSlider.js
@@ -16,6 +16,7 @@
                 this.allDelays = 'delay-0 delay-0-1 delay-0-2 delay-0-3 delay-0-4 delay-0-5 delay-0-6 delay-0-7 delay-0-8 delay-0-9 delay-1 delay-1-1 delay-1-2 delay-1-3 delay-1-4 delay-1-5 delay-1-6 delay-1-7 delay-1-8 delay-1-9 delay-2 delay-2-1 delay-2-2 delay-2-3 delay-2-4 delay-2-5 delay-2-6 delay-2-7 delay-2-8 delay-2-9 delay-3 delay-3-1 delay-3-2 delay-3-3 delay-3-4 delay-3-5 delay-3-6 delay-3-7 delay-3-8 delay-3-9 delay-4 delay-4-1 delay-4-2 delay-4-3 delay-4-4 delay-4-5 delay-4-6 delay-4-7 delay-4-8 delay-4-9 delay-5 delay-5-1 delay-5-2 delay-5-3 delay-5-4 delay-5-5 delay-5-6 delay-5-7 delay-5-8 delay-5-9 delay-6 delay-6-1 delay-6-2 delay-6-3 delay-6-4 delay-6-5 delay-6-6 delay-6-7 delay-6-8 delay-6-9 delay-7 delay-7-1 delay-7-2 delay-7-3 delay-7-4 delay-7-5 delay-7-6 delay-7-7 delay-7-8 delay-7-9 delay-8 delay-8-1 delay-8-2 delay-8-3 delay-8-4 delay-8-5 delay-8-6 delay-8-7 delay-8-8 delay-8-9 delay-9 delay-9-1 delay-9-2 delay-9-3 delay-9-4 delay-9-5 delay-9-6 delay-9-7 delay-9-8 delay-9-9 delay-10';
                 this.allEnterAnimsArr = this.allEnterAnims.split(' ');
                 this.allExitAnimsArr = this.allExitAnims.split(' ');
+                this.slideStayTime = this.settings.lastElemTime * 1000;
                 this.$slides = this.$slider.children('.' + this.settings.slideClass);
                 if (0 >= this.$slides.length) {
                     throw new Error('No elements with the CSS class "' + this.settings.slideClass + '" found!' );
@@ -76,8 +77,10 @@
                                 $elements.last().addClass(this.settings.lastElemClass);
                             }
                         } else {
-                            var delay = $(this).data('delay');
-                            $(this).addClass(delay?'delay-'+delay:'delay-0');
+                            $elements.each(function () {
+                                var delay = $(this).data('delay');
+                                $(this).addClass(delay?'delay-'+delay:'delay-0');
+                            });
                         }
                     }
                     $indicator.prepend('<span class="' + this.settings.navClass + '" data-go-to-slide="' + i + '"></span>');
@@ -128,6 +131,9 @@
                 this.$slides.eq(this.curSlide).removeClass('rolling');
                 this.curSlide = next;
                 var $nextSlide = this.$slides.eq(this.curSlide).addClass('rolling');
+                if($nextSlide.data('animateSelf')) {
+                    this.slideSelfAnimate();
+                }
                 if ($nextSlide.data('domino')) {
                     this.slideDomino();
                 } else {
@@ -136,23 +142,26 @@
                 }
             },
 
+            slideSelfAnimate: function () {
+                var that = this,
+                    $theSlide = this.$slides.eq(this.curSlide),
+                    enter = $theSlide.data('enter'),
+                    exit = $theSlide.data('exit');
+                $theSlide.addClass('animated ' + enter);
+                this.slideTime = setTimeout(function () {
+                    $theSlide.removeClass(enter).addClass(exit);
+                    this.playSlide(that.curSlide+1);
+                }, this.slideStayTime * 1000 );
+            },
+
             slideDomino: function () {
 
                 var that = this,
-                    $theSlide = this.$slides.eq(this.curSlide);
-                if($theSlide.data('animateSelf')) {
-                    var enter = $theSlide.data('enter'),
-                        exit = $theSlide.data('exit');
-                        $theSlide.addClass('animated ' + enter);
-                        this.slideTime = setTimeout(function () {
-                            $theSlide.removeClass(enter).addClass(exit);
-                            this.playSlide(that.curSlide+1);
-                        }, this.settings.lastElemTime * 1000 );
-                } else {
-                    var $elements = $theSlide.find('.' + this.settings.elemClass),
+                    $theSlide = this.$slides.eq(this.curSlide),
+                $elements = $theSlide.find('.' + this.settings.elemClass),
                         $first = $elements.filter('.' + this.settings.firstElemClass),
                         $last = $elements.filter('.' + this.settings.lastElemClass);
-                    $elements.filter('not(.' + this.settings.lastElemClass + ')').on('animationend', function () {
+                    $elements.filter(':not(.' + this.settings.lastElemClass + ')').on('animationend', function () {
                         var $nextElem = $(this).next();
                         $nextElem.addClass('animated ' + $nextElem.data('enter'));
                     });
@@ -160,7 +169,6 @@
                         that.slideDominoEnd($elements);
                     });
                     $first.addClass('animated ' + $first.data('enter'));
-                }
             },
 
             slideDominoEnd: function ($elements) {
@@ -171,7 +179,7 @@
                         $(this).removeClass($(this).data('enter')).addClass($(this).data('exit'));
                     });
                     this.playSlide(this.curSlide+1);
-                }, this.settings.lastElemTime * 1000 );
+                }, this.slideStayTime );
             },
 
             defaults :
