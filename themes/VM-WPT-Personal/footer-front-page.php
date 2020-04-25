68bbecee263f74d98c08a0d3bba4e87ac44e549d
SweetHome <49893905+vahidm90@users.noreply.github.com>
2020-04-26 03:29:16 +0430

    footer.php > added menu classes; footer-front-page.php > added menu classes; mywebsite.sql > added global footer menu.

--- themes/VM-WPT-Personal/footer-front-page.php
+++ themes/VM-WPT-Personal/footer-front-page.php
@@ -1,5 +1,5 @@
 <footer>
-	<?php wp_nav_menu( array( 'theme_location' => 'home_footer' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'home_footer', 'container_class' => 'fp-menu', 'menu_class' => 'd-flex flex-column flex-lg-row justify-content-lg-around list-unstyled pb-5 text-center' ) ); ?>
 </footer>
 </div><!-- #wrap -->
 <?php get_footer();
--- themes/VM-WPT-Personal/footer.php
+++ themes/VM-WPT-Personal/footer.php
@@ -1,6 +1,6 @@
 <?php
 
-wp_nav_menu( array( 'theme_location' => 'global_footer' ) );
+wp_nav_menu( array( 'theme_location' => 'global_footer', 'container_class' => 'footer-menu global-menu', 'menu_class' => 'd-flex flex-column flex-lg-row justify-content-lg-around list-unstyled pb-5 text-center' ) );
 
 //TODO: add cookie policy
 
